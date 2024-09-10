fbda02ba9002c999169e369a8a695f28a81fae18
Uladzislau Balan <vladislav.balan96@gmail.com>
2024-09-10 21:27:06 +0300

    4try

---
+++ PythonApplication4/PythonApplication4.py
@@ -0,0 +1,8 @@
+
+nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
+
+
+flattened_list = [digit for sublist1 in nice_list for sublist2 in sublist1 for digit in sublist2]
+
+
+print(flattened_list)
